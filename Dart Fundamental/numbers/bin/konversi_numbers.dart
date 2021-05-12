8e97595b7820a1ed48b15c9c09bcb6086a8c8555
lordacil <rizkynugrahaherliyanto28@gmail.com>
2021-05-13 04:49:57 +0700

    add dart-fundamental_numbers

---
+++ Dart Fundamental/numbers/bin/konversi_numbers.dart
@@ -0,0 +1,18 @@
+int main() {
+  // String -> int
+  var eleven = int.parse('11');
+
+  // String -> double
+  var elevenPointTwo = double.parse('11.2');
+
+  // int -> String
+  var elevenAsString = 11.toString();
+  
+  // double -> String
+  var piAsString = 3.14159.toStringAsFixed(2); // String piAsString = '3.14'
+
+  print(eleven);
+  print(elevenPointTwo);
+  print(elevenAsString);
+  print(piAsString);
+}
---
+++ Dart Fundamental/numbers/bin/numbers.dart
@@ -0,0 +1,12 @@
+import 'package:numbers/numbers.dart' as numbers;
+
+void main(List<String> arguments) {
+  // bilangan integer
+  var number = 2020;
+  var hex = 0xDEADBEEF;
+  // bilangan double
+  var decimal = 1.2;
+  var pi = 3.4;
+  double withoutDecimal = 7; // Sama dengan double withoutDecimal = 7.0
+
+}
