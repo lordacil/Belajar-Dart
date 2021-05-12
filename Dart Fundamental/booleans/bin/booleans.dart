86c038295399a8b809ebb71fef46743e743ac688
lordacil <rizkynugrahaherliyanto28@gmail.com>
2021-05-13 05:05:30 +0700

    add dart-fundamental_booleans

---
+++ Dart Fundamental/booleans/bin/booleans.dart
@@ -0,0 +1,21 @@
+import 'package:booleans/booleans.dart' as booleans;
+
+void main(List<String> arguments) {
+  
+  // boolean diambil dari nama seorang matematikawan asal inggris
+  // yaitu George Boole dikenal karena penciptaan aljabar boolean
+
+  bool alwaysTrue = true;
+  var alwaysFalse = false;
+  var notTrue = !true;
+  bool notFalse = !false;
+
+  // tanda '!' dikenal dengan not atau bang
+
+  // pengkondisian bool
+  if(true){
+    print("It's true");
+  } else{
+    print("It's False");
+  }
+}
---
+++ Dart Fundamental/booleans/test/booleans_test.dart
@@ -0,0 +1,8 @@
+import 'package:booleans/booleans.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
