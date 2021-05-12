4de50269371592e32ba5963b39b797c352204379
lordacil <rizkynugrahaherliyanto28@gmail.com>
2021-05-13 04:24:19 +0700

    add dart-fundamental_variables

---
+++ Dart Fundamental/variables/bin/variables.dart
@@ -0,0 +1,11 @@
+import 'package:variables/variables.dart' as variables;
+
+void main(){
+  // cetak berupa teks
+  var greetings = 'Hello Dart!';
+  print(greetings);
+  // cetak berupa numerik
+  var myAge;
+  myAge = 20;
+  print(myAge);
+}
